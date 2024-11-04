9313f4a188f0c5f0c9b2330fdafe8c3caf97d109
Vitor Katakura <7vitorgomez@gmail.com]>
2024-11-04 14:25:59 -0300

    página inicial finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,187 +1,56 @@
-import { createGame } from '@/actions/create-game';
-import Image from 'next/image'
-import { Coming_Soon } from 'next/font/google'
-import {
-    Carousel,
-    CarouselItem,
-    CarouselContent,
-    CarouselNext,
-    CarouselPrevious,
-} from '@/components/ui/carousel'
-import { Card, CardContent, CardTitle } from '@/components/ui/card'
-
-const comingSoon = Coming_Soon({ weight: '400', subsets: ['latin'] })
+import { createGame } from "@/actions/create-game";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-                <div className="flex-row flex w-full gap-20">
-                    <Card className="dark max-w-xs">
-                        <CardTitle
-                            className={`text-xl mt-5 font-semibold text-center ${comingSoon.className}`}
-                        >
-                            Crie o seu jogo
-                        </CardTitle>
-                        <CardContent className="flex aspect-square items-center justify-center p-6">
-                            <form
-                                className="flex flex-col w-full p-2"
-                                action={createGame}
-                            >
-                                <label className="text-xl font-semibold text-center mb-6">
-                                    Escreva um nickname e cria a sua sala
-                                </label>
-                                <input
-                                    className="text-black mb-3 p-1 rounded-lg"
-                                    type="text"
-                                    name="name"
-                                    required
-                                />
-                                <button className="font-semibold" type="submit">
-                                    Create Game
-                                </button>
-                            </form>
-                        </CardContent>
-                    </Card>
-                    <div className="flex flex-col justify-center">
-                        <Carousel className="w-full max-w-xs dark">
-                            <CarouselContent>
-                                <CarouselItem>
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardTitle
-                                                className={`text-xl mt-5 font-semibold text-center ${comingSoon.className}`}
-                                            >
-                                                Como jogar
-                                            </CardTitle>
-                                            <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span
-                                                    className={`text-2xl font-semibold ${comingSoon.className}`}
-                                                >
-                                                    Crie uma sala e compartilhe
-                                                    com seus amigos o link que
-                                                    será gerado
-                                                </span>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                                <CarouselItem>
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardTitle
-                                                className={`text-xl mt-5 font-semibold text-center ${comingSoon.className}`}
-                                            >
-                                                Como jogar
-                                            </CardTitle>
-                                            <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span
-                                                    className={`text-2xl font-semibold ${comingSoon.className}`}
-                                                >
-                                                    Quando estiver pronto, é só
-                                                    começar o jogo
-                                                </span>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                                <CarouselItem>
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardTitle
-                                                className={`text-xl mt-5 font-semibold text-center ${comingSoon.className}`}
-                                            >
-                                                Como jogar
-                                            </CardTitle>
-                                            <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span
-                                                    className={`text-2xl font-semibold ${comingSoon.className}`}
-                                                >
-                                                    Na primeira rodada escolha o
-                                                    nome da sua tirinha e comece
-                                                    o desenho do primeiro
-                                                    quadrinho
-                                                </span>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                                <CarouselItem>
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardTitle
-                                                className={`text-xl mt-5 font-semibold text-center ${comingSoon.className}`}
-                                            >
-                                                Como jogar
-                                            </CardTitle>
-                                            <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span
-                                                    className={`text-2xl font-semibold ${comingSoon.className}`}
-                                                >
-                                                    Na rodada seguinte, outra
-                                                    pessoa vai colocar os balões
-                                                    de fala no seu quadrinho e
-                                                    assim por diante
-                                                </span>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                            </CarouselContent>
-                            <CarouselPrevious />
-                            <CarouselNext />
-                        </Carousel>
-                    </div>
-                </div>
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
+  return (
+    <div className="flex flex-col w-full items-center h-[90vh] gap-8">
+      <Image src="/logo.svg" alt="Logo" width={560} height={240} className="mt-10" />
+      <main className="flex gap-[10px]">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-2xl font-semibold">Crie o seu jogo</h1>
+          <div className="w-[450px] h-[450px] border-4 border-zinc-900 relative">
+            <Image src={"/rick.svg"} alt="Rick" width={435} height={300} className="absolute -z-10 bottom-0" />
+            <form className="flex flex-col h-full justify-center items-center p-2" action={createGame}>
+              <label className="font-semibold text-center text-wrap text-3xl w-4/5 mb-4">
+                Escreva um nickname e crie a sua sala
+              </label>
+              <input
+                className="text-black mb-3 w-3/5 p-1 rounded-lg border-2 border-zinc-900"
+                type="text"
+                name="name"
+                required
+              />
+              <button className="font-semibold text-xl hover:underline" type="submit">
+                Criar sala
+              </button>
+            </form>
+          </div>
         </div>
-    )
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-2xl font-semibold">Como jogar</h1>
+          <div className="flex flex-wrap w-[450px] gap-[10px] text-center text-xl relative">
+            <Image
+              src={"/rick2.svg"}
+              alt="Rick"
+              width={43}
+              height={100}
+              className="absolute -z-10 -top-[70px] right-4"
+            />
+            <div className="w-[220px] h-[220px] border-4 border-zinc-900 flex items-center justify-center p-4">
+              Crie uma sala e compartilhe com seus amigos o link que será gerado
+            </div>
+            <div className="w-[220px] h-[220px] border-4 border-zinc-900 flex items-center justify-center p-4">
+              Quando estiver pronto, é só começar o jogo
+            </div>
+            <div className="w-[220px] h-[220px] border-4 border-zinc-900 flex items-center justify-center p-4">
+              Na primeira rodada, escolha o nome da sua tirinha e comece o desenho do primeiro quadrinho
+            </div>
+            <div className="w-[220px] h-[220px] border-4 border-zinc-900 flex items-center justify-center p-4">
+              Na rodada seguinte, outra pessoa vai colocar os balões de fala no seu quadrinho e assim por diante :D
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
