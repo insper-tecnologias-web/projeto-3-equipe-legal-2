9f2c00cebf76ad30ef01124dca5ad537cadd5b49
Vitor Katakura <7vitorgomez@gmail.com]>
2024-11-07 08:46:38 -0300

    remove prisma

--- prisma/migrations/20241105231419_add_cascade_effect_to_games/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "players" DROP CONSTRAINT "players_gameId_fkey";
-
--- AddForeignKey
-ALTER TABLE "players" ADD CONSTRAINT "players_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "games"("gameId") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/app/api/games/[gameId]/route.ts
+++ src/app/api/games/[gameId]/route.ts
@@ -1,7 +1,6 @@
 import { database } from "@/lib/firebase";
-import { prisma } from "@/lib/prisma";
 import { get, ref } from "firebase/database";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request: NextResponse, { params }: { params: Promise<{ gameId: string }> }) {
   const { gameId } = await params;
@@ -15,30 +14,6 @@ export async function GET(request: NextResponse, { params }: { params: Promise<{
     }
 
     return NextResponse.json(snapShot, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Erro interno do servidor" }, { status: 500 });
-  }
-}
-
-export async function PUT(request: NextRequest, { params }: { params: Promise<{ gameId: string }> }) {
-  const { gameId } = await params;
-
-  try {
-    const game = await prisma.game.update({
-      where: {
-        gameId,
-      },
-      data: {
-        status: "PLAYING",
-      },
-    });
-
-    if (!game) {
-      return NextResponse.json({ error: "Jogo não encontrado" }, { status: 404 });
-    }
-
-    return NextResponse.json(game);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Erro interno do servidor" }, { status: 500 });
